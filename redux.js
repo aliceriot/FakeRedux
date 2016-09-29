45e23fee1a6a3c58260f87b2a378158b7d685a61
Alice Pote <alice.writes.wrongs@gmail.com>
2016-09-29 10:26:42 -0400

    starting to write a redux thing for fun! not so hard

---
+++ redux.js
@@ -0,0 +1,35 @@
+/*
+ * It's redux! I'm reimplementing it for fun.
+ */
+
+const Redux = reducer => {
+  let store = reducer()
+
+  return {
+    dispatch: action => store = reducer(store, action),
+    getState: () => store,
+  }
+}
+
+const selfOrEmpty = obj => (
+  obj === undefined ? {} : obj
+)
+
+const exampleReducer = (state, action) => {
+  state = selfOrEmpty(state)
+  action = selfOrEmpty(action)
+  switch (action.type) {
+    case 'FIRST_TYPE':
+      return Object.assign({}, state, {
+        firstType: action.payload
+      })
+    case 'SECOND_TYPE':
+      return Object.assign({}, state, {
+        secondType: action.payload
+      })
+    default:
+      return state
+  }
+}
+
+module.exports = { Redux, exampleReducer }
